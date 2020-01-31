1f7863a74f97d48a28cbf11366ed4c9f4803f898
arzafr <rezaarnanda080101@gmail.com>
2020-01-31 05:50:27 -0800

    modul5

---
+++ mod5.js
@@ -0,0 +1,73 @@
+//variabel
+//1
+var variable1 = '23
+var variable2 = 23
+
+//2
+let variable3 = '34'
+let variable4 34
+
+//3
+variable5 = '65'
+variable6 = 65
+
+
+//fungsi
+//1
+function namafungsi(){
+
+}
+
+//2
+var tambah = function(){
+    return a + b
+}
+tambah(2, 3)
+//3
+var kurang =  (a, b) => {
+    return a - b
+}
+kurang(1, 2)
+
+
+//percabangan
+//1
+x = 6
+if(x ,7){
+
+} else if(x > 9) {
+
+} else {
+
+}
+
+//2
+switch(x){
+    case 5 : 'Lima'
+    case 6 : 'enam'
+    default : 'else'
+}
+//3
+x < 7 ? 'True' : x > 8 ? 'True' : 'False'
+
+
+//perulangan
+//1
+for(let i = 0; i < 10; i++){
+    console.log(array[i])
+}
+
+//2
+for (item in array){
+    console.log(item)
+}
+
+//3
+do {
+
+}while(x <3)
+
+
+
+
+console.log(variable1)
